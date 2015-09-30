220fa76f7cd39cadeb808b48043760a03a8f33ab
Robbie Pallas <robbie.pallas@piksel.com>
2015-09-30 17:13:12 +0100

    added search test (failing) and moved nodiak specific code to test file

--- test/clients/nodiak.js
+++ test/clients/nodiak.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
-var Lab = require('lab');
+var Async = require('async');
 var Code = require('code');
-var DbSupport = require('./../_support/db');
+var Lab = require('lab');
 
 // Test shortcuts
 
@@ -14,30 +14,16 @@ var describe = lab.describe;
 var it = lab.it;
 var expect = Code.expect;
 
-// Database
-
-var db = DbSupport.db;
-before(DbSupport.before);
-// beforeEach(DbSupport.beforeEach);
-// afterEach(DbSupport.afterEach);
-
-// System under test
-
-var poc = require('../../lib/index');
-
-
-before((done) => {
-  done();
-});
-
-after((done) => {
-  done();
-});
-
 describe('Nodiak', () => {
+  var riak;
+
+  beforeEach((done) => {
+    riak = require('nodiak').getClient('http', 'localhost', 11098);
+    done();
+  });
 
   it('should ping riak', (done) => {
-    db.ping((err, result) => {
+    riak.ping((err, result) => {
       expect(err).to.not.exist();
       expect(result).to.equal('OK');
       done();
@@ -45,7 +31,7 @@ describe('Nodiak', () => {
   });
 
   it('should retrieve stats from riak', (done) => {
-    db.stats((err, result) => {
+    riak.stats((err, result) => {
       expect(result).to.exist();
       expect(result.connected_nodes).to.exist();
       expect(result.ring_members).to.exist();
@@ -54,7 +40,7 @@ describe('Nodiak', () => {
   });
 
   it('should store a key/value in riak', (done) => {
-    var things = db.bucket('things').objects;
+    var things = riak.bucket('things').objects;
     var thing = things.new('owner:name', {thing: 'value'});
     things.save(thing, (err, result) => {
       expect(result).to.exist();
@@ -65,7 +51,7 @@ describe('Nodiak', () => {
   });
 
   it('should delete a key/value from riak', function (done) {
-    var things = db.bucket('things').objects;
+    var things = riak.bucket('things').objects;
     var thing = things.new('owner:name', { thing: 'value'});
     things.save(thing, (/*err, result*/) => {
       things.delete(thing, (err, result) => {
@@ -76,11 +62,67 @@ describe('Nodiak', () => {
     });
   });
 
-  it('should retrieve a maximal from riak', (done) => {
-    db.bucket('maximals').objects.get(['test:foo'], (err, result) => {
-      var maximal = result.shift().data;
-      expect(maximal.name).to.equal('foo');
-      done();
+  describe('With maximals', () => {
+
+    var seedData = require('../_support/seeds/maximals.json');
+
+    beforeEach((done) => {
+      Async.series([
+        clearDownData,
+        runSeeds
+      ], done);
+    });
+
+    var clearDownData = (done) => {
+      var maximals = riak.bucket('maximals').objects;
+      Async.auto({
+        maximals: (callback) => {
+          maximals.all(callback);
+        },
+        deletes: ['maximals', (callback, results) => {
+          maximals.delete(results.maximals, callback);
+        }]
+      }, done);
+    };
+
+    var runSeeds = (done) => {
+      var data = [];
+      var maximals = riak.bucket('maximals').objects;
+      _.take(seedData.maximals, 6).forEach((maximal) => {
+        data.push(maximals.new(`${maximal.owner}:${maximal.name}`, maximal));
+      });
+      maximals.save(data, done);
+    };
+
+    it('should retrieve a maximal from riak', (done) => {
+      riak.bucket('maximals').objects.get(['test:foo'], (err, result) => {
+        var maximal = result.shift().data;
+        expect(maximal.name).to.equal('foo');
+        done();
+      });
+    });
+
+    it('should retrieve multiple maximals from riak', (done) => {
+      var keys = ['test:foo', 'test:bar', 'test:baz', 'test:quux', 'test:zoo', 'root:foo'];
+      riak.bucket('maximals').objects.get(keys, (err, result) => {
+        expect(result.length).to.equal(6);
+        expect(_.find(result, { data: { owner: 'test', name: 'foo'}})).to.exist();
+        expect(_.find(result, { data: { name: 'bar'}})).to.exist();
+        expect(_.find(result, { data: { name: 'baz'}})).to.exist();
+        expect(_.find(result, { data: { name: 'quux'}})).to.exist();
+        expect(_.find(result, { data: { name: 'zoo'}})).to.exist();
+        expect(_.find(result, { data: { owner: 'root', name: 'foo'}})).to.exist();
+        done();
+      });
+    });
+
+    // TODO: have to setup indexes manually with curl - how do we do this with nodiak?
+    it('should retrieve a maximal from riak using solr search', {skip: true}, (done) => {
+      riak.bucket('maximals').search.solr({ q: 'name:b*' }, (err, result) => {
+        // FIXME: figure out how to do this
+        expect(JSON.parse(result.toString()).response.numFound).to.equal(2);
+        done();
+      });
     });
   });
 
