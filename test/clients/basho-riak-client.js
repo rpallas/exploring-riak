46fba393b7b899e40ce39c14e4aaa496d327f464
Robbie Pallas <robbie.pallas@piksel.com>
2015-10-05 01:25:46 +0100

    some basics with basho client

--- test/clients/basho-riak-client.js
+++ test/clients/basho-riak-client.js
@@ -15,49 +15,53 @@ var describe = lab.describe;
 var it = lab.it;
 var expect = Code.expect;
 
-describe('Basho Riak Client', () => {
-  var riak = new Riak.Client([
-    'localhost:11098'
-  ]);
+describe('Basho Riak Client', {skip: false}, () => {
+  var riak;
 
   before((done) => {
+    riak = new Riak.Client([
+      '192.168.99.100:32769',
+      '192.168.99.100:32771',
+      '192.168.99.100:32773',
+      '192.168.99.100:32775',
+      '192.168.99.100:32777'
+    ]);
     done();
-  });
-
-  after((done) => {
-    riak.shutdown((state) => {
-      if (state === Riak.Cluster.State.SHUTDOWN) {
-        done();
-      }
-    });
   });
 
   it('should ping riak', (done) => {
     riak.ping((err, result) => {
-      console.log(err, result);
-      //expect(result).to.equal(true);
+      expect(result).to.equal(true);
       done(err);
     });
   });
 
-  it('should retrieve stats from riak', (done) => {
-    //TODO
-    done();
-  });
-
-  it('should store a key/value in riak', (done) => {
-    //TODO
-    done();
+  it('should store and fetch a key/value in riak', (done) => {
+    riak.storeValue({ bucket: 'things', key: 'owner:name', value: {thing: 'value'}}, (err, result) => {
+      expect(result).to.exist();
+      riak.fetchValue({ bucket: 'things', key: 'owner:name', convertToJs: true}, (err, result) => {
+        expect(result.values).not.to.be.empty();
+        expect(result.values[0].value.thing).to.equal('value');
+        done(err);
+      });
+    });
   });
 
   it('should delete a key/value from riak', (done) => {
-    //TODO
-    done();
+    riak.storeValue({ bucket: 'things', key: 'owner:name', value: {thing: 'value'}}, (err, result) => {
+      expect(result).to.exist();
+      riak.deleteValue({ bucket: 'things', key: 'owner:name'}, () => {
+        riak.fetchValue({ bucket: 'things', key: 'owner:name', convertToJs: true}, (err, result) => {
+          expect(result.values).to.be.empty();
+          done(err);
+        });
+      });
+    });
   });
 
   describe('With maximals', () => {
 
-    var seedData = require('../_support/seeds/maximals.json');
+    var maximalSeeds = _.take(require('../_support/seeds/maximals.json').maximals, 6);
 
     beforeEach((done) => {
       Async.series([
@@ -67,18 +71,27 @@ describe('Basho Riak Client', () => {
     });
 
     var clearDownData = (done) => {
-      //TODO
-      done();
+      Async.each(maximalSeeds, (maximal, callback) => {
+        riak.deleteValue({ bucket: 'maximals', key: `${maximal.owner}:${maximal.name}`}, callback);
+      }, done);
     };
 
     var runSeeds = (done) => {
-      //TODO
-      done();
+      Async.each(maximalSeeds, (maximal, callback) => {
+        riak.storeValue({
+          bucket: 'maximals',
+          key: `${maximal.owner}:${maximal.name}`,
+          value: maximal
+        }, callback);
+      }, done);
     };
 
     it('should retrieve a maximal from riak', (done) => {
-      //TODO
-      done();
+      riak.fetchValue({ bucket: 'maximals', key: 'test:foo', convertToJs: true}, (err, result) => {
+        expect(result.values).not.to.be.empty();
+        expect(result.values[0].value.name).to.equal('foo');
+        done(err);
+      });
     });
 
     it('should retrieve multiple maximals from riak', (done) => {
